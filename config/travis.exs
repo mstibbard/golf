45200013010b4e47905c80e407fcf974ba2a7146
Matthew Stibbard <matthewstibbard@gmail.com>
2018-09-28 23:24:18 +1000

    force xenial image in travis

--- config/travis.exs
+++ config/travis.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :golf, Golf.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "golf_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
