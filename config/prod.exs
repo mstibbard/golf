89a4569a632fd9222ee8755b278ee0636ede67fe
Matthew Stibbard <matthewstibbard@gmail.com>
2019-03-16 20:29:51 -0700

    preparing production config for use with gatling

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :golf, GolfWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [host: "example.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -50,9 +51,9 @@ config :logger, level: :info
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
+
+config :phoenix, :serve_endpoints, true
+
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
 #
